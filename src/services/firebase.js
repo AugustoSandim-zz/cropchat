0334ab72eb7353325b5dc98261030620c783b836
Augusto Sandim <augusto@jera.com.br>
2017-06-18 21:40:42 -0400

    Sending data for database and build post

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,16 @@
-import firebase from ‘firebase’;
+import firebase from 'firebase'
 
 var config = {
-    apiKey: "AIzaSyDBFi4NzAfVMTmqGaImWNVzD9GguLH3PQ0",
-    authDomain: "cropchat-58201.firebaseapp.com",
-    databaseURL: "https://cropchat-58201.firebaseio.com",
-    projectId: "cropchat-58201",
-    storageBucket: "cropchat-58201.appspot.com",
-    messagingSenderId: "417970353525"
-  };
-  firebase.initializeApp(config);
+  apiKey: 'AIzaSyDBFi4NzAfVMTmqGaImWNVzD9GguLH3PQ0',
+  authDomain: 'cropchat-58201.firebaseapp.com',
+  databaseURL: 'https://cropchat-58201.firebaseio.com',
+  projectId: 'cropchat-58201',
+  storageBucket: 'cropchat-58201.appspot.com',
+  messagingSenderId: '417970353525'
+}
+
+firebase.initializeApp(config)
 
 export default {
- database: firebase.database()
+  database: firebase.database()
 }
